4bee8450540eb6c3c2f0d5ec55b22afbac8732ea
Abir <royabirlebachun0@gmail.com>
2022-12-09 12:50:27 +0530

    Enabling windows close and maximize button and modifying code of video_face_detection.

--- video_face_detection.py
+++ video_face_detection.py
@@ -11,21 +11,34 @@ cap = cv2.VideoCapture('testvid.mp4')
 # read first frame
 success , img = cap.read()
 
-while (success):
+while (True):
     # Read the frame
     success , img = cap.read()
+    # To resize the camera window and to use maximize button
+    cv2.namedWindow("img", cv2.WINDOW_NORMAL)
     # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    try:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    except:
+        break
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
     # Display
     cv2.imshow('img', img)
     # Stop if escape key is pressed
-    k = cv2.waitKey(30) & 0xff
+    k = cv2.waitKey(1) & 0xff
     if k==27:
         break
+    # When cross button is clicked the window closed.
+    # That is its a alternate quit button
+    try:
+        if(cv2.getWindowProperty('img', 0) < 0):
+            break
+    except:
+        break
+
 # Release the VideoCapture object
 cap.release()
